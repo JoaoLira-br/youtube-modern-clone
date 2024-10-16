d1eed22e5f1a36ee72d3222aa4ef199e6baf2cd6
Joao Victor Lira <lira440440@gmail.com>
2024-10-16 15:16:35 -0300

    implements channel detail page

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,27 +4,44 @@ import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    // this e.preventDefault() is to block the page from refreshing: not what we want with reactjs
+    e.preventDefault();
+    if(searchTerm){
+      // redirect to the search page
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+    }
+
+  }
+
   return (
-
-        // this white div that looks like it is floating
-      <Paper
-        component={"form"}
-        onSubmit={() => {}}
-        sx={{
-          borderRadius: 20,
-          border: "1px solid #e3e3e3",
-          pl: 2,
-          boxShadow: "none",
+    // this white div that looks like it is floating
+    <Paper
+      component={"form"}
+      onSubmit={handleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
         //   this { sm: 5 } is a conditional margin right only on small screens
-          mr: { sm: 5 },
-        }}
-      >
-        <input type="text" className="search-bar" placeholder="Search..." value={""} onChange={()=> {}}/>
-        <IconButton type="submit" sx={{ p: 1 }}>
-            <Search />
-        </IconButton>
-      </Paper>
-
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type="submit" sx={{ p: 1 }}>
+        <Search />
+      </IconButton>
+    </Paper>
   );
 };
 
