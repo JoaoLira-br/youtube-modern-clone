d22adf8b842dd522626f180533fe8d17dd5c4c44
Joao Victor Lira <lira440440@gmail.com>
2024-10-28 18:43:57 -0300

    adds real time voice detection to search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,55 +1,46 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import { Search, Mic } from "@mui/icons-material";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const { transcript, resetTranscript } = useSpeechRecognition();
-  const [listening, setListening] = useState(false);
+  const { transcript, listening, resetTranscript } = useSpeechRecognition();
   const navigate = useNavigate();
+
+  // Update search term in real-time as the user speaks
+  useEffect(() => {
+    setSearchTerm(transcript);
+  }, [transcript]);
+
   const handleSubmit = (e) => {
-    // this e.preventDefault() is to block the page from refreshing: not what we want with reactjs
     e.preventDefault();
-    if(searchTerm){
-      // redirect to the search page
+    if (searchTerm) {
       navigate(`/search/${searchTerm}`);
       setSearchTerm('');
+      resetTranscript();
     }
-
-  }
+  };
 
   const handleMic = () => {
-    if(listening){
+    if (listening) {
       SpeechRecognition.stopListening();
-      setListening(false);
-      if(transcript){
-        setSearchTerm(transcript);
-        console.log(searchTerm);
-        resetTranscript();
-        
-      }
-  }else{
-    SpeechRecognition.startListening({ language: 'en-US' });
-    setListening(true);
-  }
-}
-  useEffect(() => {
+    } else {
+      resetTranscript();
+      SpeechRecognition.startListening({ language: 'en-US', continuous: false });
+    }
+  };
 
-  })
-  
   return (
-    // this white div that looks like it is floating
     <Paper
-      component={"form"}
+      component="form"
       onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
         pl: 2,
         boxShadow: "none",
-        //   this { sm: 5 } is a conditional margin right only on small screens
         mr: { sm: 5 },
       }}
     >
@@ -63,10 +54,9 @@ const SearchBar = () => {
       <IconButton type="submit" sx={{ p: 1 }}>
         <Search />
       </IconButton>
-      <IconButton type="button" sx={{ p: 1, color: (listening && 'red')}} onClick={handleMic} >
+      <IconButton type="button" sx={{ p: 1, color: (listening && 'red') }} onClick={handleMic} onTouchEnd={handleMic}>
         <Mic />
       </IconButton>
-      
     </Paper>
   );
 };
